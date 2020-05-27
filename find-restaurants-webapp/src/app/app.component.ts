30c7bf5bfd6c8bf3b69449eca67fc832353422e1
sabarinath <sabarinathsri@gmail.com>
2020-05-27 10:34:37 +0530

    Integrated GetRestaurants ws

--- find-restaurants-webapp/src/app/app.component.ts
+++ find-restaurants-webapp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -6,26 +6,39 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'restaurantApplication';
 
   loadedRestaurants  = [];
 
   constructor(private http: HttpClient) {}
 
+  ngOnInit(){
+    this.fetchRestaurants();
+  }
+  
   onCreateRestaurant(restaurantData:{name:string, rating:string, type:string}){
-    
-    this.http.post("http://localhost:8080/restaurants", restaurantData)
-    .subscribe(responseData => {
+    this.http
+    .post("http://localhost:8080/restaurants", restaurantData)
+    .subscribe( responseData => {
       console.log(responseData);
-
-    });
+      }
+    );
   }
 
   onFetchRestaurants(){
-    
+    this.fetchRestaurants();
   }
   onClearRestaurants() {
     // Send Http request
+  }
+
+  private fetchRestaurants(){
+    this.http
+    .get("http://localhost:8080/restaurants")
+    .subscribe( restaurants => {
+        console.log(restaurants);
+      }
+    );
   }
 }
