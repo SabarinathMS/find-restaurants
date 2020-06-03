523ee52f68b1b6a08380f4b968e3250fdd856607
sabarinath <sabarinathsri@gmail.com>
2020-06-03 19:43:49 +0530

    Response file

--- find-restaurants-api/src/main/java/com/demo/restaurant/rest/responses/RestaurantResponse.java
+++ find-restaurants-api/src/main/java/com/demo/restaurant/rest/responses/RestaurantResponse.java
@@ -1,33 +1,5 @@
 package com.demo.restaurant.rest.responses;
 
-public class RestaurantResponse {
-	private Integer id;
-	private String name;
-	private String type;
-	private String rating;
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public String getRating() {
-		return rating;
-	}
-	public void setRating(String rating) {
-		this.rating = rating;
-	}
-	
-}
+import com.demo.restaurant.model.Restaurant;
+
+public class RestaurantResponse extends Restaurant {}
